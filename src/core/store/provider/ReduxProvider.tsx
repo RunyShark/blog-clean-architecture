22c8aec10a96d4e7a8648ff5c7a221c318d36ed2
RunyShark <spartadragon117@live.com.mx>
2024-02-17 07:53:27 -0600

    FEAT Redux-RTK

---
+++ src/core/store/provider/ReduxProvider.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import { store } from '../store';
+import { BaseComponentProps } from '@/core/interface';
+import { Provider } from 'react-redux';
+
+export const ReduxProvider: React.FC<Pick<BaseComponentProps, 'children'>> = ({
+  children,
+}) => {
+  return <Provider store={store}>{children}</Provider>;
+};
---
+++ src/core/store/slices/web/web-thunk.ts
@@ -0,0 +1,14 @@
+export class WebThunk {
+  //todo implement
+  private mapperResponse() {}
+
+  public getBlog() {
+    this.mapperResponse();
+  }
+
+  public postBlog() {
+    this.mapperResponse();
+  }
+}
+
+export const webThunk = new WebThunk();
