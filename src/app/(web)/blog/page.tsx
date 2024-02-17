c93f89b2e837af236aac0ce063e04b63f7f53299
RunyShark <spartadragon117@live.com.mx>
2024-02-17 14:55:28 -0600

    FEAT Card component

--- src/app/(web)/blog/page.tsx
+++ src/app/(web)/blog/page.tsx
@@ -1,19 +1,15 @@
-import { Button, Header } from '@/presentation/components';
+import { Button, Card, Header } from '@/presentation/components';
 
 export default function BlogPage() {
   return (
     <>
       <Header />
-      <div className="flex-1 screen">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, iste
-          nisi? Ipsum earum et minima aliquid, blanditiis amet quas repudiandae
-          hic dolorem, quisquam tempore veritatis eveniet alias, sit explicabo
-          beatae!
-        </p>
-        <Button iconLeft={undefined} iconRight={undefined}>
-          wenas
-        </Button>
+      <div className="flex-1 screen flex-col gap-16 mb-16">
+        <div>
+          <h1>Search</h1>
+          <Button />
+        </div>
+        <Card />
       </div>
     </>
   );
